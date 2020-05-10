3f3203c2a576df74e32fc71fda9bea17bd2dad41
Henrique Melanda <hjdesigner23@gmail.com>
2020-05-10 00:34:12 -0300

    Finalizando nosso cliente

--- src/api/clientes.js
+++ src/api/clientes.js
@@ -1,19 +1,24 @@
-import { api, opcoesFetch } from './config'
+import { opcoesFetch } from './config'
 
 
 
 const listarClientes = () => 
-  fetch('http://localhost:4000', opcoesFetch('{clientes { nome cpf }}'))
+  fetch('http://localhost:4000', opcoesFetch('{clientes { id nome cpf }}'))
     .then(resposta => resposta.json())
     .then(dados => dados.data.clientes)
 
 const buscarClientePorId = id => 
-  api
-    .get(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data[0])
+  fetch('http://localhost:4000', opcoesFetch(`{
+    cliente(id: ${id}) {
+      nome
+      cpf
+    } 
+  }`))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data.cliente)
 
 const adicionarCliente = cliente => 
-  fetch('http://localhost:4000'. opcoesFetch(`
+  fetch('http://localhost:4000', opcoesFetch(`
     mutation {
       adicionarCliente(nome: "${cliente.nome}", cpf: "${cliente.cpf}"){
         id
@@ -22,17 +27,27 @@ const adicionarCliente = cliente =>
     }
   `))
     .then(resposta => resposta.json())
-    .then(dados => dados.data.cliente)
+    .then(dados => dados.data)
 
 const alterarCliente = (id, cliente) =>
-  api
-    .put(`/clientes/cliente/${id}`, cliente)
-    .then(resposta => resposta.data)
+  fetch('http://localhost:4000', opcoesFetch(`
+    mutation {
+      atualizarCliente(id: ${id}, nome: "${cliente.nome}", cpf: "${cliente.cpf}"){
+        nome
+      }
+    }
+    `))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data)
 
 const removerCliente = id => 
-  api
-    .delete(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data)
+  fetch('http://localhost:4000', opcoesFetch(`
+    mutation {
+      deletarCliente(id: ${id})
+    }
+    `))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data)
 
 export default {
   listarClientes,
